b063e416e25f5eb127da1d8a1efd3957d46db226
emexbazz <bmexit@gmail.com>
2021-06-09 18:31:00 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,220 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Networth App',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Color(0xff222747),
+        textSelectionTheme: TextSelectionThemeData(
+          cursorColor: Colors.grey.shade800,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            padding: EdgeInsets.symmetric(horizontal: 56, vertical: 16),
+          ),
+        ),
+        cardTheme: CardTheme(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        ),
+        cardColor: Color(0xff444968),
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var assetAmount = 0;
+  var liabilitiesAmount = 0;
+
+  void setAssetAmount(int asset) {
+    setState(() {
+      assetAmount = asset;
+    });
+  }
+
+  void setLiabilitiesAmount(int liabilities) {
+    setState(() {
+      liabilitiesAmount = liabilities;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      body: SafeArea(
+        //SafeArea prevents a its children widgets from going behind sin sometinglike the AppBAr
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(height: 48),
+                Text(
+                  'Add your assets and liabilities',
+                  style: theme.textTheme.headline5,
+                ),
+                SizedBox(height: 102),
+                AmountCard(
+                  title: 'Assets',
+                  amount: assetAmount,
+                  onTap: () => showDialog(
+                    context: context,
+                    builder: (context) {
+                      return NumberInputDialog(
+                        onTap: setAssetAmount,
+                        title: 'Assets',
+                        amount: assetAmount,
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(height: 16),
+                AmountCard(
+                  title: 'Liabilities',
+                  amount: liabilitiesAmount,
+                  onTap: () => showDialog(
+                    context: context,
+                    builder: (context) {
+                      return NumberInputDialog(
+                        onTap: setLiabilitiesAmount,
+                        title: 'Liabilities',
+                        amount: liabilitiesAmount,
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AmountCard extends StatelessWidget {
+  const AmountCard({
+    Key? key,
+    required this.title,
+    required this.amount,
+    this.onTap,
+  }) : super(key: key);
+  final String title;
+  final int amount;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      height: 148,
+      width: size.width * 0.8,
+      child: Card(
+        child: InkWell(
+          onTap: onTap,
+          borderRadius: BorderRadius.circular(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(title, style: Theme.of(context).textTheme.bodyText1),
+              SizedBox(height: 4),
+              Text(amount.toString(),
+                  style: Theme.of(context).textTheme.headline6),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class NumberInputDialog extends StatefulWidget {
+  const NumberInputDialog({
+    Key? key,
+    required this.onTap,
+    required this.title,
+    required this.amount,
+  }) : super(key: key);
+
+  final Function(int) onTap;
+  final String title;
+  final int amount;
+
+  @override
+  _NumberInputDialogState createState() => _NumberInputDialogState();
+}
+
+class _NumberInputDialogState extends State<NumberInputDialog> {
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final outlineInputBorder = OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.grey.shade600, width: 1),
+    );
+
+    return Dialog(
+      backgroundColor: Colors.white,
+      child: Container(
+        padding: EdgeInsets.all(20),
+        width: screenSize.width * 0.7,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              widget.title,
+              style: Theme.of(context)
+                  .accentTextTheme
+                  .headline6
+                  ?.copyWith(color: Colors.grey.shade800),
+            ),
+            SizedBox(height: 24),
+            TextField(
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              style: TextStyle(color: Colors.grey.shade900),
+              autofocus: true,
+              decoration: InputDecoration(
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                labelStyle: TextStyle(color: Colors.grey.shade600),
+                focusedBorder: outlineInputBorder,
+                enabledBorder: outlineInputBorder,
+                labelText: 'Write Amount',
+              ),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Theme.of(context).cardColor,
+                padding: EdgeInsets.symmetric(horizontal: 56),
+              ),
+              onPressed: () {},
+              child: Text(
+                'Done',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
